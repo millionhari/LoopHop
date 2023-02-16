baaf2a9293602e9c78d509b8f4c8f0760923e06f
Michael Hari <mikemhari@gmail.com>
2023-02-15 22:45:11 -0800

    Different images

--- App.js
+++ App.js
@@ -9,6 +9,9 @@ import {
   Modal,
   Image,
 } from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+const Stack = createNativeStackNavigator();
 
 const styles = StyleSheet.create({
   container: {
@@ -33,57 +36,53 @@ const styles = StyleSheet.create({
   },
 });
 
+const Hylia = () => {
+  return (
+    <Image
+      source={{
+        uri: 'https://i0.wp.com/www.mattvince.com/wp-content/uploads/2019/04/Lake-Hylia_thumb.jpg?fit=768%2C432&ssl=1',
+      }}
+      style={{width: '100%', height: '100%'}}
+    />
+  );
+};
+
+const Forest = () => {
+  return (
+    <Image
+      source={{
+        uri: 'https://wallpaperaccess.com/full/42613.jpg',
+      }}
+      style={{width: '100%', height: '100%'}}
+    />
+  );
+};
 class App extends React.Component {
   state = {
     posts: [],
   };
 
-  componentDidMount() {
-    fetch('https://www.reddit.com/r/pic/top.json?t=year')
-      .then(response => response.json())
-      .then(json => {
-        const posts = json.data.children.map(child => child.data);
-        this.setState({posts});
-      });
-  }
-
   render() {
     return (
-      <ScrollView contentContainerStyle={styles.container}>
-        {this.state.posts.map(post => (
-          <View style={styles.tile} key={post.id}>
-            <TouchableHighlight
-              style={styles.highlight}
-              underlayColor="#a8dadc"
-              // We use onPress to open Modal and to set selected image url to state
-              onPress={() =>
-                this.setState({modalVisible: true, selectedImage: post.url})
-              }>
-              <ImageBackground
-                style={{width: '100%', height: '100%'}}
-                source={{uri: post.thumbnail}}
-                imageStyle={styles.background}
-              />
-            </TouchableHighlight>
-            <Text style={styles.title}>{post.title}</Text>
-          </View>
-        ))}
-        <Modal
-          animationType={'fade'}
-          transparent={true}
-          visible={this.state.modalVisible}
-          onRequestClose={() => this.setState({modalVisible: false})}>
-          {/*there is a bug in react native tv os: modal will not close properly, unless you wrap it's content into TouchableHighlight */}
-          <TouchableHighlight
-            activeOpacity={1}
-            onPress={() => this.setState({modalVisible: false})}>
-            <Image
-              source={{uri: this.state.selectedImage}}
-              style={{width: '100%', height: '100%'}}
-            />
-          </TouchableHighlight>
-        </Modal>
-      </ScrollView>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen
+            name="hylia"
+            component={Hylia}
+            options={{
+              title: 'lake hylia',
+            }}
+            onPress={() => navigation.navigate('forest', {title: 'forest'})}
+          />
+          <Stack.Screen
+            name="forest"
+            component={Forest}
+            options={{
+              title: 'forest',
+            }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
     );
   }
 }
